0783091d48ab527668bcd2d06d27a7b0fcd68533
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2026-06-18 16:03:35 +0530

    just checking nvim

--- Week-2-JavaScript-Basics/basic-assignments/Functions.js
+++ Week-2-JavaScript-Basics/basic-assignments/Functions.js
@@ -5,3 +5,23 @@ function greet(name) {
 }
 
 greet("Ayush");
+
+// Question-2: Write a function that takes two parameters, adds them together, and returns the result.
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log("3+5 = " + sum(3, 5));
+
+// Question-3: Write a function that takes the current hour as a parameter and logs a different greeting message based on whether it's morning, afternoon, or evening.
+
+function greeting(time) {
+    if (time >= 5 && time < 12) console.log("Good Morning!");
+    else if (time >= 12 && time <= 15) console.log("Good Afternoon!");
+    else console.log("Good Evening!");
+}
+
+greeting(8);
+greeting(12);
+greeting(16);
