4271f74001b485b5a1c95704f3d280b31cd55c94
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2026-06-18 23:44:56 +0530

    Loops questions

---
+++ Week-2-JavaScript-Basics/basic-assignments/Loops.js
@@ -0,0 +1,29 @@
+// Question-1: WAP that logs numbers from 1 to 10 to the console.
+
+console.log("1 to 10 :");
+for (let i = 1; i <= 10; i++) console.log(i);
+console.log("\n");
+
+// Question-2: WAP that log all even numbers from 2 to 20.
+
+console.log("2 to 20 :");
+for (let i = 2; i <= 20; i++) console.log(i);
+
+// Question-3: WAP that counts backward from 10 to 1 and logs the values.
+
+console.log("10 to 1 :");
+for (let i = 10; i > 0; i--) console.log(i);
+
+// Question-4: WAP to calculate the sum of numbers from 1 to 5.
+
+let sum = 0;
+for (let i = 0; i <= 5; i++) sum += i;
+
+console.log("Sum = " + sum);
+
+// Question-5: WAP that prints powers of 2 (2^n) up to 64.
+
+console.log("Power = ");
+for (let i = 0; i <= 6; i++) console.log(2 ** i);
+
+// Question-6: WAP to Develop a do-while loop for a simple number guessing game. Ask the user to guess a number between 1 and 10, and keep prompting until they guess correctly.
