cd87f2c095065603b82869f31feef4447bd62cc1
SinghAyush07 <200604790+SinghAyush07@users.noreply.github.com>
2026-06-20 21:48:19 +0530

    learning objects

--- Week-2-JavaScript-Basics/basic-assignments/Objects.js
+++ Week-2-JavaScript-Basics/basic-assignments/Objects.js
@@ -14,8 +14,34 @@ function deepCopy(obj) {
 let obj = {
     name: "Ayush",
     age: 22,
-    gender: "Male",
+    gender: "male",
 };
 
 const newObj = deepCopy(obj);
 console.log(newObj);
+
+// Question-2: Write a function that takes two objects and returns an object containing the common key-value pairs.
+
+let obj1 = {
+    name: "Ayush",
+    age: 22,
+    gender: "male",
+    address: "Ranchi",
+};
+
+function commonKeyValue(obj, obj1) {
+    let newObject = {};
+    for (let key in obj) {
+        if (obj1.hasOwnProperty(key) && obj[key] === obj1[key]) {
+            newObject[key] = obj[key];
+        }
+    }
+    return newObject;
+}
+
+let newObject = commonKeyValue(obj, obj1);
+console.log(newObject);
+
+// Question-3: Create a function that transforms an object by renaming its keys based on a provided mapping. The function should handle nested objects. For example:
+
+function transfromObj() { }
